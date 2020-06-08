e445ce4b6193787ddb74aadb4c50c2584ac20a02
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-08 16:46:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return name;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return name;
+}
+
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return name;
+}
+
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return name;
+}
+
+function appendKitten(name) {
+  kittens[...kittens,name];
+  return kittens;
+}
